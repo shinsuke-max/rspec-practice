cae1b164e3d2103a7878b295da53081a9b2e2374
EC2 Default User <ec2-user@ip-172-31-33-247.ap-northeast-1.compute.internal>
2020-03-05 11:41:33 +0000

    add test related factory_bot

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -43,4 +43,27 @@ RSpec.describe Project, type: :model do
     other_project.valid?
     expect(other_project).to be_valid
   end
+  
+  describe "late status" do
+    # 締め切り日が過ぎていれば遅れていること
+    it "is late when the due date is past today" do
+      project = FactoryBot.create(:project, :due_yesterday)
+      expect(project).to be_late
+    end
+    
+    it "is on time when the due date is today" do
+      project = FactoryBot.create(:project, :due_today)
+      expect(project).to_not be_late
+    end
+    
+    it "is on time when the due date is in the future" do
+      project = FactoryBot.create(:project, :due_tomorrow)
+      expect(project).to_not be_late
+    end
+  end
+  
+  it "can have many notes" do
+    project = FactoryBot.create(:project, :with_note)
+    expect(project.notes.length).to eq 5
+  end
 end
